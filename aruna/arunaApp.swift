bbde8f23a59447151afc296d88051518f5902daf
Romadhan Prasetyo <romadhanedy@gmail.com>
2026-08-09 19:22:40 +0700

    Initial Commit

---
+++ aruna/arunaApp.swift
@@ -0,0 +1,17 @@
+//
+//  arunaApp.swift
+//  aruna
+//
+//  Created by Romadhan Edy on 09/08/26.
+//
+
+import SwiftUI
+
+@main
+struct arunaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
